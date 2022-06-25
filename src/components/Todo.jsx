a9c01cf564228dbb572ad9f9ab336c17509e5804
Leo Ho <1876595827qq@gmail.com>
2022-06-26 04:59:00 +1200

    fix 'React has detected a change in the order of Hooks called'

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
 import deleteButton from "../assets/icon-cross.svg";
 import checkIcon from "../assets/icon-check.svg";
 
 export default function List({ todos, handleDelete }) {
   // map list element for every task in the todos list
   const list = todos.map((item, index) => {
-    const [showCross, setShowCross] = useState(false);
-
     return (
-      <li
-        className="relative border-b border-darkBorder bg-darkSecondary cursor-grab text-darkText1"
-        key={index}
-        onMouseEnter={() => setShowCross(true)}
-        onMouseLeave={() => setShowCross(false)}
-      >
+      <li className="relative border-b group border-darkBorder bg-darkSecondary cursor-grab text-darkText1" key={index}>
         {/* check button */}
         <button className="p-[0.4rem] mx-8 rounded-full aspect-square bg-gradient-to-r from-checkFrom to-checkTo bg-check">
           <img src={checkIcon} alt="check-icon" />
@@ -23,11 +15,13 @@ export default function List({ todos, handleDelete }) {
         <p className="inline-block py-5">{item.todo}</p>
 
         {/* delete button */}
-        {showCross && (
-          <button type="button" className="absolute flex-1 right-8 top-6" onClick={() => handleDelete(index, item.id)}>
-            <img src={deleteButton} alt="delete button" />
-          </button>
-        )}
+        <button
+          type="button"
+          className={`invisible group-hover:visible absolute flex-1 right-8 top-6`}
+          onClick={() => handleDelete(index, item.id)}
+        >
+          <img src={deleteButton} alt="delete button" />
+        </button>
       </li>
     );
   });
